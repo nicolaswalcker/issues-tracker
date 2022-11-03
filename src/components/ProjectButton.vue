11cf838ede45ab705320105bac7747f3e7717fe1
Nicolas Walcker <walcker_@outlook.com>
2022-11-03 19:30:07 -0300

    feat: create project button

---
+++ src/components/ProjectButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button" @click="isClicked" :disabled="isDisabled">
+    <span class="icon">
+      <i :class="buttonIcon"></i>
+    </span>
+    <span>{{ buttonText }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProjectButton',
+  emits: ['isClicked'],
+  props: {
+    isDisabled: {
+      type: Boolean,
+    },
+    buttonIcon: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    isClicked(): void {
+      this.$emit('isClicked');
+    },
+  },
+});
+</script>
