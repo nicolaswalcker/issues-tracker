bbb7c01e0a03c6d6304a237b093622e860668cf7
Nicolas Walcker <walcker_@outlook.com>
2022-11-03 19:29:56 -0300

    feat: create project task

---
+++ src/components/ProjectTask.vue
@@ -0,0 +1,28 @@
+<template>
+  <ProjectBox class="box has-text-weight-bold">
+    <section class="columns">
+      <article class="column is-7">
+        {{ task.description || 'Tarefa sem descrição' }}
+      </article>
+      <ProjectStopwatch :time="task.durationInSecs" />
+    </section>
+  </ProjectBox>
+</template>
+
+<script lang="ts">
+import ITask from '@/interfaces/ITask';
+import { defineComponent, PropType } from 'vue';
+import ProjectBox from './ProjectBox.vue';
+import ProjectStopwatch from './ProjectStopwatch.vue';
+
+export default defineComponent({
+  name: 'ProjectTask',
+  components: { ProjectStopwatch, ProjectBox },
+  props: {
+    task: {
+      type: Object as PropType<ITask>,
+      required: true,
+    },
+  },
+});
+</script>
