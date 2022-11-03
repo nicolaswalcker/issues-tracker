eeb960de47513d3aa2ad3aa31f72c0abd53bb736
Nicolas Walcker <walcker_@outlook.com>
2022-11-03 19:29:47 -0300

    feat: create generic box

---
+++ src/components/ProjectBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <article class="box has-text-weight-bold"><slot /></article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProjectBox',
+});
+</script>
+
+<style scoped>
+.box {
+  background: lemonchiffon;
+}
+</style>
